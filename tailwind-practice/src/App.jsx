198dd0bab0a670dac3c2c6a636ac8888618f3a07
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-11-05 19:00:16 -0500

    Update App.jsx

--- tailwind-practice/src/App.jsx
+++ tailwind-practice/src/App.jsx
@@ -5,8 +5,10 @@ import './App.css'
 
 function App() {
   return (
-    <div className="border-4 border-gray-500 text-3xl font-bold text-blue-600 text-center mt-10">
-      Tailwind is working! **
+    <div className="min-h-screen bg-gray-700 flex items-center justify-center">
+      <h1 className="shadow-lg bg-yellow-200 border-4 border-gray-500 rounded-lg text-3xl font-bold text-blue-600 text-center mt-10">
+        Tailwind is working! **
+      </h1>
     </div>
   );
 }
