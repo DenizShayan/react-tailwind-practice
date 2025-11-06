c990380c63651aa2e117427aed8ff04e605eeecb
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-11-05 22:34:26 -0500

    Update App.jsx

--- tailwind-practice/src/App.jsx
+++ tailwind-practice/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonVariants from './components/ButtonVariants'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-700 flex items-center justify-center">
-      <h1 className="shadow-lg bg-yellow-200 border-4 border-gray-500 rounded-lg text-3xl font-bold text-blue-600 text-center mt-10">
+      <h1 className="shadow-lg bg-yellow-200 border-2 border-gray-800 rounded-lg text-3xl font-bold text-blue-600 text-center mt-10">
         Tailwind is working! **
       </h1>
+    <ButtonVariants />
     </div>
   );
 }
