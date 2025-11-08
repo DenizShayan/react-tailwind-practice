a54745db72aa6a4cdd069396ce7a3df9f4dfa51f
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-11-07 20:55:54 -0500

    Update ButtonVariants.jsx

--- tailwind-practice/src/components/ButtonVariants.jsx
+++ tailwind-practice/src/components/ButtonVariants.jsx
@@ -22,6 +22,15 @@ function ButtonVariants() {
                 <button className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600" >secondary </button>
                 <button className="bg-yellow-400 text-black px-4 py-2 rounded-md hover:bg-yellow-500" >warning </button>
             </div>
+
+            {/* third row of different buttons */}
+            <div className="flex justify-center gap-4">
+                <button disabled className="bg-gray-500 text-white px-4 py-2 rounded-md opacity-70 cursor-not-allowed">Disabled</button>
+                <button className="bg-blue-600 text-white px-4 py-2 flex items-center rounded-md gap-2">
+                    <span className="animation-spin border-2 border-white border-t-transparent rounded-full w-4 h-4"></span>
+                    Loading...
+                </button>
+            </div>
         </div>
     );
 }
